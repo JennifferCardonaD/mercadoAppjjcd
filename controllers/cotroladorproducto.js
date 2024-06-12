7eb785ac5f98e47ad1496799e5bbecea81a37be5
juanjlopera <jramirezj@cesde.net>
2024-06-12 17:34:02 -0500

    actualizacion

---
+++ controllers/cotroladorproducto.js
@@ -0,0 +1,17 @@
+let url" localhost:8080/storeapi/v1/producto"
+
+let peticion={
+    method:"GET",
+    Headers:{}    
+}
+
+fetch(url,peticion)
+.then(function(respuesta){
+    return respuesta.json()
+})
+.then(function(respuesta){
+    console.log(respuesta)
+})
+.catch(function(respuesta){
+    console.log(respuesta)
+})
